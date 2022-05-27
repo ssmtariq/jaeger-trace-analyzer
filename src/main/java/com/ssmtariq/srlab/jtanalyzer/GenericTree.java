011b2baee4048d087d5a9f099a249612c7728b19
ssmtariq <syedtariqfiles@gmail.com>
2022-05-27 12:23:44 -0400

    Formatted result display

--- src/main/java/com/ssmtariq/srlab/jtanalyzer/GenericTree.java
+++ src/main/java/com/ssmtariq/srlab/jtanalyzer/GenericTree.java
@@ -81,7 +81,7 @@ public class GenericTree {
     }
 
     public static void displayServiceAggregatedInfo(Map<String, Integer> spanCounter, Map<String, Double> serviceDuration){
-        System.out.printf("%-25s %-5s %-15s \n", "SERVICE", "SPAN", "DURATION(seconds)");
+        System.out.printf("%-27s %-6s %-15s \n", "SERVICE", "SPAN", "DURATION(seconds)");
         System.out.println("-------------------------------------------------");
         final int[] counter = {0};
         spanCounter.forEach((k,v)->{
