c495037f1ba31662195bef6aaf288b2a34c157e0
ssmtariq <syedtariqfiles@gmail.com>
2022-05-27 15:43:06 -0400

    Update result display format

--- src/main/java/com/ssmtariq/srlab/jtanalyzer/GenericTree.java
+++ src/main/java/com/ssmtariq/srlab/jtanalyzer/GenericTree.java
@@ -132,7 +132,7 @@ public class GenericTree {
     public static void aggregateResult(Map<String, Double> serviceDuration) {
         serviceDuration.forEach((k,v)->{
             if (aggregatorMap.containsKey(k)) {
-                aggregatorMap.put(k, (long) (v+aggregatorMap.get(k)));
+                aggregatorMap.put(k, (v.longValue()+aggregatorMap.get(k)));
             } else {
                 aggregatorMap.put(k, v.longValue());
             }
