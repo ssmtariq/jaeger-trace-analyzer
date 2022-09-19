00f07f84e4548907e0af6f572637bc68dd8c6aa4
ssmtariq <ssmtariq@ms1301.utah.cloudlab.us>
2022-09-19 06:55:51 -0600

    Update configurations for travel-plan searchQuickestRoute

--- src/main/java/com/ssmtariq/srlab/jtanalyzer/Constants.java
+++ src/main/java/com/ssmtariq/srlab/jtanalyzer/Constants.java
@@ -3,7 +3,7 @@ package com.ssmtariq.srlab.jtanalyzer;
 public final class Constants {
     public static final String ES_SCHEMA = "http";
     /* Set elasticsearch host */
-    public static final String ES_HOST = "192.168.161.248";
+    public static final String ES_HOST = "192.168.226.66";
     public static final Integer ES_PORT = 9200;
     public static final String ES_INDEX = "jaeger-span-*";
 
@@ -21,12 +21,12 @@ public final class Constants {
     public static final String TIME_ZONE = "America/Detroit";
     public static final String TIME_FIELD = "startTimeMillis";
     /* Set request start and end time */
-    public static final String START_TIME = Utility.dateToMilliseconds("2022/09/12 10:02:00");
-    public static final String END_TIME = Utility.dateToMilliseconds("2022/09/12 10:12:00");
+    public static final String START_TIME = Utility.dateToMilliseconds("2022/09/16 16:43:00");
+    public static final String END_TIME = Utility.dateToMilliseconds("2022/09/16 16:56:00");
     /* Set service name you wanna analyze */
     public static final String SERVICE_PRESERVE_OTHER = "ts-travel-plan-service";
 
     /* Check JaegerUI and set number of total services invoked by the intended service */
-    public static final Integer NUMBER_OF_SERVICE_COUNT = 8;
+    public static final Integer NUMBER_OF_SERVICE_COUNT = 13;
     public static final Integer SUCCESS_REQUESTS_TO_RETRIEVE = 0;
 }
